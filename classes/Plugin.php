a58a8a9e84f1f5d375083bedcfde950ded9e55c9
Christoph M. Becker <cmbecker69@gmx.de>
2017-03-30 00:34:52 +0200

    Extract class

---
+++ classes/Plugin.php
@@ -0,0 +1,54 @@
+<?php
+
+/**
+ * Copyright 2017 Christoph M. Becker
+ *
+ * This file is part of Fa_XH.
+ *
+ * Fa_XH is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Fa_XH is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Fa_XH.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace Fa;
+
+class Plugin
+{
+    private static $isEmitted = false;
+
+    public static function emitLink()
+    {
+        global $hjs, $pth;
+    
+        if (self::$isEmitted) {
+            return;
+        }
+        self::$isEmitted = true;
+        $pluginFolder = "{$pth['folder']['plugins']}fa/";
+        $hjs .= <<<HTML
+<style type="text/css">
+@font-face {
+  font-family: 'FontAwesome';
+  src: url('{$pluginFolder}fonts/fontawesome-webfont.eot?v=4.7.0');
+  src: url('{$pluginFolder}fonts/fontawesome-webfont.eot?#iefix&v=4.7.0') format('embedded-opentype'),
+       url('{$pluginFolder}fonts/fontawesome-webfont.woff2?v=4.7.0') format('woff2'),
+       url('{$pluginFolder}fonts/fontawesome-webfont.woff?v=4.7.0') format('woff'),
+       url('{$pluginFolder}fonts/fontawesome-webfont.ttf?v=4.7.0') format('truetype');
+  font-weight: normal;
+  font-style: normal;
+}
+</style>
+<link rel="stylesheet" type="text/css" href="{$pluginFolder}css/font-awesome.css">
+
+HTML;
+    }
+}
--- classes/required_classes.php
+++ classes/required_classes.php
@@ -19,7 +19,9 @@
  * along with Fa_XH.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-function fa_link()
-{
-    Fa\Plugin::emitLink();
-}
+spl_autoload_register(function ($class) {
+    $parts = explode('\\', $class, 2);
+    if ($parts[0] == 'Fa') {
+        include_once __DIR__ . '/' . $parts[1] . '.php';
+    }
+});
