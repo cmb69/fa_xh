0629f465b20f37973f7d3b2ada9e05d9e2f58756
Christoph M. Becker <cmbecker69@gmx.de>
2017-03-30 18:15:23 +0200

    Fix logo alt text

--- languages/en.php
+++ languages/en.php
@@ -1,6 +1,6 @@
 <?php
 
-$plugin_tx['fa']['alt_logo']="Awesome flag";
+$plugin_tx['fa']['alt_logo']="Fort Awesome";
 
 $plugin_tx['fa']['cf_require_auto']="Whether Font Awesome is always available.";
 
