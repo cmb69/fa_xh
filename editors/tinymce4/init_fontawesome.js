14fc5b80a87dbd06d81c295b667003bcbb29bf42
Christoph M. Becker <cmbecker69@gmx.de>
2017-04-15 12:02:07 +0200

    Fix URLs in init_fontawesome.js
    Commit 99a4c72 renamed fontawesome.css to fontawesome.min.css, which we
    have to cater to in init_fontawesome.js.

--- editors/tinymce4/init_fontawesome.js
+++ editors/tinymce4/init_fontawesome.js
@@ -16,7 +16,7 @@
   image_advtab: true,
   image_title: true,
   file_browser_callback: "%FILEBROWSER_CALLBACK%",
-  content_css: "%STYLESHEET%,plugins/fa/css/font-awesome.css,../plugins/fa/css/font-awesome.css",
+  content_css: "%STYLESHEET%,plugins/fa/css/font-awesome.min.css,../plugins/fa/css/font-awesome.min.css",
   importcss_append:true,
 //inserted 30.11.2016  
   style_formats_autohide: true,
