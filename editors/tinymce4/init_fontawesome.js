f1eed5f1697da9a34d2117f5554e695c094a1404
Christoph M. Becker <cmbecker69@gmx.de>
2017-04-15 12:27:21 +0200

    Close #12: TinyMCE4 plugin: allow TinyMCE to be loaded from CDN
    We solve that by loading the fontawesome plugin as external plugin,
    which has the additional benefit that the user neither has to fiddle
    with TinyMCE's required_classes.php, nor has to copy the fontawesome
    plugin into TinyMCE's plugin folder. Instead they only have to adjust a
    path in init_fontawesome.js.

--- editors/tinymce4/init_fontawesome.js
+++ editors/tinymce4/init_fontawesome.js
@@ -9,6 +9,9 @@
     "image importcss insertdatetime link lists media nonbreaking paste",
     "save searchreplace table textcolor visualblocks visualchars wordcount fontawesome noneditable"
   ],
+  external_plugins: {
+    "fontawesome": "%CMSIMPLE_ROOT%plugins/fa/editors/tinymce4/fontawesome/plugin.min.js"
+  },
   toolbar1: "save | fullscreen code formatselect fontselect fontsizeselect styleselect fontawesome",
   toolbar2: "bold italic underline strikethrough | alignleft aligncenter alignright alignjustify cut copy paste pastetext | bullist numlist outdent indent blockquote",
   toolbar3: "undo redo | link unlink anchor image media | hr nonbreaking removeformat visualblocks visualchars | forecolor backcolor | searchreplace | charmap",
