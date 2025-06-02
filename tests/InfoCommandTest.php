2236937aedab6e15b30eb5890d1a3505c899565f
Christoph M. Becker <cmbecker69@gmx.de>
2025-06-02 20:06:44 +0200

    Test InfoCommand

---
+++ tests/InfoCommandTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Fa;
+
+use ApprovalTests\Approvals;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+
+class InfoCommandTest extends TestCase
+{
+    private View $view;
+
+    protected function setUp(): void
+    {
+        $this->view = new View("./views/", XH_includeVar("./languages/en.php", "plugin_tx")["fa"]);
+    }
+
+    /** @return InfoCommand&MockObject */
+    private function sut()
+    {
+        return $this->getMockBuilder(InfoCommand::class)
+            ->setConstructorArgs(["./plugins/fa/", $this->view])
+            ->onlyMethods(["compareVersions", "isWritable"])
+            ->getMock();
+    }
+
+    public function testRendersPluginInfo(): void
+    {
+        Approvals::verifyHtml($this->sut()());
+    }
+}
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -8,3 +8,5 @@ require_once "./classes/Plugin.php";
 require_once "./classes/InfoCommand.php";
 require_once "./classes/RequireCommand.php";
 require_once "./classes/View.php";
+
+const CMSIMPLE_XH_VERSION = "CMSimple_XH 1.7.5";
